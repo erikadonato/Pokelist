7ccadc113e0f1eac6d74d7b8e1b72520c4073852
Erika Donato <erikaadonato@gmail.com>
2021-12-15 21:45:43 -0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,12 @@
+var xhr = new XMLHttpRequest(); 
+
+xhr.onreadystatechange = function () {
+    if( xhr.readyState ==  4 && xhr.status == 200 ) {
+        res = xhr.response;
+        res = JSON.parse(res);
+        console.log(res)
+    }
+}
+
+xhr.open("GET", "URL_AQUI");
+xhr.send();
