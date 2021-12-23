3d22b7827f30c2643aaa384a29e7f426a278c919
Erika Donato <erikaadonato@gmail.com>
2021-12-23 19:20:11 -0300

    update

--- script/script.js
+++ script/script.js
@@ -1,12 +1,30 @@
-var xhr = new XMLHttpRequest(); 
+const fetchPokemon = () => {
+    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+    const pokemonsList = []
 
-xhr.onreadystatechange = function () {
-    if( xhr.readyState ==  4 && xhr.status == 200 ) {
-        res = xhr.response;
-        res = JSON.parse(res);
-        console.log(res)
+    for ( let i = 1; i <= 150; i++ ) {
+        pokemonsList.push(
+            fetch(getPokemonUrl(i))
+            .then(response => response.json())
+        )
     }
+
+    Promise.all(pokemonsList)
+        .then(pokemons => {
+            console.log(pokemons)
+            const listPokemons = pokemons.reduce((accumulator, pokemon) => {
+                accumulator += 
+                `
+                <li class="card>
+                    <img class="card-image ${}" /> 
+                    <h2 class="card-title">${pokemon.id}.${pokemon.name}</h2>
+                    <p class="card-subtitle>${pokemon.types.map(typeInfo => typeInfo.type.name).join(' | ')}</p>
+                    </li>`
+                return accumulator
+            }, '')
+            console.log(listPokemons)
+        })
+
 }
 
-xhr.open("GET", "URL_AQUI");
-xhr.send();
+fetchPokemon()
